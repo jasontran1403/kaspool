b7c5bca5a512ec952501447752e8b144f1374375
jasontran1403 <71239283+jasontran1403@users.noreply.github.com>
2024-10-26 18:52:25 +0700

    update 3

--- src/components/WalletCard2.jsx
+++ src/components/WalletCard2.jsx
@@ -24,7 +24,7 @@ const WalletCard2 = ({ content, amount, unit, rank, wallet }) => {
         {/* Button section - Centered */}
         {rank ?? (
           <div className="flex justify-center mt-4 md:mt-6">
-            <a
+            {/* <a
               href={isAdmin ? "/admin/deposit-kaspool/"+id : "/deposit-kaspool"}
               className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-gray rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
@@ -35,7 +35,7 @@ const WalletCard2 = ({ content, amount, unit, rank, wallet }) => {
               className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-gray rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
               Withdraw
-            </a>
+            </a> */}
             <a
               href={isAdmin ? "/admin/transfer/"+id : "/transfer"}
               className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-gray rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
